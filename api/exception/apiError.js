0be240e369c0fe9f3df1e81fefeb16d970e7822b
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-07-01 11:00:44 +0300

    add missing params error function

--- api/exception/apiError.js
+++ api/exception/apiError.js
@@ -11,4 +11,9 @@ module.exports = class ApiError extends Error {
     static badRequest(message, errors = []) {
         return new ApiError(message, 400, errors);
     }
+
+    static missingParams(errors = {}) {
+        const message = 'Missing required function params!';
+        return new ApiError(message, 400, errors);
+    }
 }
--- api/services/authService.js
+++ api/services/authService.js
@@ -1,18 +1,26 @@
 const bcrypt = require('bcrypt');
+const ApiError = require('../exception/apiError');
 const UserModel = require('../models/User');
 
 class AuthService {
     async registration(username, email, password) {
+        if (!username || !email || !password ) {
+            throw ApiError.missingParams({ username, email, password });
+        }
 
         const hashPass = await bcrypt.hash(password, 5);
 
-        const user = await UserModel.create({
+        return UserModel.create({
            username,
            email,
            password: hashPass
         });
+    }
 
-        return user;
+    async login(email, password) {
+        if (!email || !password) {
+            throw ApiError.missingParams({ email, password });
+        }
     }
 }
 
