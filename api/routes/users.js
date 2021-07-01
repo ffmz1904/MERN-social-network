3dd3649c430e019828472c6ff90426280779041c
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-07-01 11:19:34 +0300

    update user model

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,10 @@ const Router = require('express').Router;
 const userController = require('../controllers/userController');
 const router = new Router();
 
-
+//  update user
+//  delete user
+//  get user
+//  follow user
+//  unfollow
 
 module.exports = router;
