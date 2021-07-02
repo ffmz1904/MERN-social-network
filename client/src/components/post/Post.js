d3d21e96493b709e4546f11085c12f1cae7fc24c
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-07-03 00:20:48 +0300

    add posibility to like/unlike post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,9 +1,18 @@
-import './post.css';
 import { MoreVert } from '@material-ui/icons';
+import {useState} from "react";
+import './post.css';
 
 import { Users } from '../../tmpData';
 
 const Post = ({ post }) => {
+    const [like, setLike] = useState(post.like);
+    const [isLiked, setIsLiked] = useState(false);
+
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1);
+        setIsLiked(!isLiked);
+    };
+
     const user = Users.filter(u => u.id === post.userId).shift();
     return (
         <div className="post">
@@ -24,9 +33,9 @@ const Post = ({ post }) => {
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className="likeIcon" src="/assets/like.png" alt=""/>
-                        <img className="likeIcon" src="/assets/heart.png" alt=""/>
-                        <span className="likeCounter">{ post.like } people like it</span>
+                        <img className="likeIcon" src="/assets/like.png" onClick={likeHandler} alt=""/>
+                        <img className="likeIcon" src="/assets/heart.png" onClick={likeHandler} alt=""/>
+                        <span className="likeCounter">{ like } people like it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">{ post.comment } comments</span>
