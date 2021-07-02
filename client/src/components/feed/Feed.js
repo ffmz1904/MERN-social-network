18e288af7ae982ff72fe5d83a4794f6843a64dba
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-07-02 23:24:45 +0300

    add post component

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,11 +1,18 @@
 import './feed.css';
 import Share from "../share/Share";
+import Post from "../post/Post";
 
 const Feed = () => {
     return (
         <div className="feed">
             <div className="feedWrapper">
                 <Share />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
             </div>
         </div>
     );
