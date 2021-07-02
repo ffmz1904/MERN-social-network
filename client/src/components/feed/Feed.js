4a78313814c5a2d9d2068503146f5eeb842209b4
Mark Zavatskyi <64529908+ffmz1904@users.noreply.github.com>
2021-07-02 23:00:40 +0300

    add share component

--- client/src/components/feed/Feed.js
+++ client/src/components/feed/Feed.js
@@ -1,9 +1,12 @@
 import './feed.css';
+import Share from "../share/Share";
 
 const Feed = () => {
     return (
         <div className="feed">
-            feed
+            <div className="feedWrapper">
+                <Share />
+            </div>
         </div>
     );
 };
